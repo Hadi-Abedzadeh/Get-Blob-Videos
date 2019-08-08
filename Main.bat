3309c6ec3ad57fe722b983aaf1e84d16b25dba31
Hadi Abedzadeh <Hadi-Abedzadeh@users.noreply.github.com>
2019-08-08 11:56:38 +0430

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -17,30 +17,28 @@ IF NOT EXIST %DOWNLOAD_DIRECTORY% ( MKDIR %DOWNLOAD_DIRECTORY% )
 
 :RUN
 COLOR %DEFAULT_COLOR%
+FOR /f %%# in ('wMIC PATH Win32_LocalTime Get /Format:value') DO @for /f %%@ IN ("%%#") DO @set %%@
 
 :STRINGS
 SET STR_M3U8="* Enter the M3U8 URL: "
 SET STR_REFERER="* Enter a valid referer URL: "
-SET STR_FILE_NAME="* Enter file name after save: "
 
 :INPUTS
 SET /p USER_AGENT=<%CONFIG_DIRECTORY%/user-agent
 SET /p M3U8_URL=%STR_M3U8%
 SET /p REFERER=%STR_REFERER%
-SET /p FILE_NAME=%STR_FILE_NAME%
 
-SET DOWNLOAD_PATH=%DOWNLOAD_DIRECTORY%/%FILE_NAME%.mp4
+SET DOWNLOAD_PATH=%DOWNLOAD_DIRECTORY%/%YEAR%%MONTH%%DAY%-%HOUR%%MINUTE%%SECOND%.mp4
 
 :LEECH
 SET COMMAND="%CD%/%STREAMLINK%/bin/streamlink.exe" --http-header "User-Agent=%USER_AGENT%" --http-header "Referer=%REFERER%" "hlsvariant://%M3U8_URL%" best -o "%DOWNLOAD_PATH%"
 
 START cmd.exe  /C "MODE con:cols=80 lines=20 && COLOR %LEECH_WINDOW_COLOR% && %COMMAND%"
 
-
 ECHO.
 COLOR %WORKING_COLOR%
 
-ECHO "Press any key to leech the next part"
+ECHO *Press any key to leech the next part
 PAUSE >nul
 
 GOTO run
